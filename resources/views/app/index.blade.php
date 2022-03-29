6d4b22bb1d0f2afcf4cbc944f1711fc4f85972a1
ErmishinD <ermishin.d.e@gmail.com>
2022-03-29 15:40:30 +0300

    shop_id from session

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,53 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <!-- Tell the browser to be responsive to screen width -->
-      <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-      <meta name="user-id" content="{{ Auth::user()->id }}">
-      <meta name="company-id" content="{{ Auth::user()->company_id }}">
-      <meta name="user_name" content="{{ Auth::user()->name }}">
-      <meta name="is_superadmin" content="{{ strval(Auth::user()->hasRole('Super Admin')) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="user-id" content="{{ Auth::user()->id }}">
+    <meta name="company-id" content="{{ Auth::user()->company_id }}">
+    <meta name="user_name" content="{{ Auth::user()->name }}">
+    <meta name="is_superadmin" content="{{ strval(Auth::user()->hasRole('Super Admin')) }}">
+    @auth
+        <meta name="shop_id" content="{{ session()->get('shop_id') }}">
+    @endauth
 
-      <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>
-            @hasSection('htmlheader_title') @yield('htmlheader_title') @endif
-        </title>
+    <title>
+        @hasSection('htmlheader_title') @yield('htmlheader_title') @endif
+    </title>
 
-        @section('styles')
-            @include('app.connect_styles')
-        @show
-      <script>
+    @section('styles')
+        @include('app.connect_styles')
+    @show
+    <script>
         @auth
             window.Permissions = {!! json_encode(Auth::user()->getAllPermissions()->pluck('name'), true) !!}
-        @else
+            @else
             window.Permissions = [];
         @endauth
     </script>
-    </head>
+</head>
 
-    <body>
-        <!-- <a style="padding-left: 295px;" class="dropdown-item" href="{{ route('logout') }}"
+<body>
+<!-- <a style="padding-left: 295px;" class="dropdown-item" href="{{ route('logout') }}"
            onclick="event.preventDefault();
                          document.getElementById('logout-form').submit();">
             {{ __('Logout') }}
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+    </a>
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                                 @csrf
-                                            </form> -->
-        <div id="app">
+    </form> -->
+<div id="app">
 
-            <app></app>
+    <app></app>
 
-        </div>
+</div>
 
-        @section('scripts')
-            @include('app.connect_scripts')
-        @show
+@section('scripts')
+    @include('app.connect_scripts')
+@show
 
-    </body>
+</body>
 
