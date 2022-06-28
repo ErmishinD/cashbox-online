d24421b26d5017ae48cd5d755c3329165a494df2
zlodey509 <76591663+zlodey509@users.noreply.github.com>
2022-06-28 21:16:40 +0300

    report charts

--- resources/assets/js/components/reports/profits/Index.vue
+++ resources/assets/js/components/reports/profits/Index.vue
@@ -4,22 +4,36 @@
   </div>
   <Datepicker class="datepicker" v-model="date" range multiCalendars :format="format"  @update:modelValue="handleInternal"></Datepicker>
 
-  <div class="tac content_title">
-    {{$t('Суммарная прибыль')}}: {{sum_data.sum_profit}}
+  <div class="tac content_title" style="    display: flex;
+    justify-content: center;
+    align-items: center;">
+    <label for="">{{$t('Магазин')}}:</label>
+    <select class="form-control" name="" id="" v-model="current_shop">
+      <option value="">{{$t('Все')}}</option>
+      <option v-for="shop in shops" :value="shop.id">{{shop.name}}</option>
+    </select>
   </div>
 
-  <div v-for="shop in data">
-    <div class="tac content_title">
-      {{shop.name}}
-    </div>
-    <div class="row-btw">
-      <span>{{$t('Доходы')}}: {{shop.sum_amount}}</span>
-      <span>{{$t('Расходы')}}: {{shop.sum_self_cost}}</span>
-      <span>{{$t('Прибыль')}}: {{shop.sum_profit}}</span>
-    </div>
+  <div style="display: flex; justify-content:space-around;">
+    <div><div class="tac content_title">{{$t('Доходы')}}</div><LineChart :chartData="amountData" :options="options" /></div>
+    <div><div class="tac content_title">{{$t('Расходы')}}</div><LineChart :chartData="expenseData" :options="options" /></div>
+    <div><div class="tac content_title">{{$t('Прибыль')}}</div><LineChart :chartData="profitData" :options="options" /></div>
   </div>
+
+  <div class="tac content_title">
+    {{$t('Прибыль по категориям')}}
+  </div>
+
+  <PieChart :chartData="profitDataByCategory" :options="categoryOptions" />
+
+  
+ 
+
+ 
 
 </template>
+
+
 
 
 
@@ -28,19 +42,42 @@
   import Datepicker from '@vuepic/vue-datepicker';
   import '@vuepic/vue-datepicker/dist/main.css'
 
+  import { LineChart } from 'vue-chart-3';
+  import { PieChart } from 'vue-chart-3';
+  import { Chart, ChartData, ChartOptions, registerables } from "chart.js";
+
+  Chart.register(...registerables);
+
 export default {
-  components: { Datepicker },
+  components: { Datepicker,
+  LineChart, PieChart },
   data(){
     return {
       date:'',
       data: '',
-      sum_data: ''
+      current_shop: '',
+      shops: '',
+      chartData: '',
+      options: '',
+      amountData: '',
+      expenseData: '',
+      profitData: '',
+      profitDataByCategory: '',
+      categoryOptions: '',
     };
   },
   watch:{
-
+    current_shop:{
+      handler(){
+        this.getDataByDate()
+      },
+      deep: true
+    }
   },
   mounted(){
+    this.axios.post('/api/get_for_select/', {entities: ['Shop']}).then(response => {
+      this.shops = response.data.Shop
+    })
     this.getDataByDate()
     
 
@@ -50,10 +87,85 @@ export default {
   methods:{
       getDataByDate(startDate, endDate){
         this.emitter.emit("isLoading", true);
-        this.axios.get('/api/reports/profit', {params: {start_date: startDate, end_date:endDate}}).then((response) => {
-            this.data = response.data['data']
-            this.sum_data = response.data['sum_data']
+        this.axios.get(`/api/reports/profit_by_day/`, {params: {start_date: startDate, end_date:endDate, shop_id: this.current_shop}}).then((response) => {
+            Promise.resolve(this.data = response.data['data']).then(() => {
+              this.amountData = {
+                labels: this.data.date_list,
+                datasets: [
+                  {
+                    label: this.$t('Доходы'),
+                    data: this.data.amount_list,
+                    fill: true,
+                    borderColor: "#ffc107",
+                  },
+                  
+                ],
+              }
+
+              this.expenseData = {
+                labels: this.data.date_list,
+                datasets: [
+                  {
+                    label: this.$t('Расходы'),
+                    data: this.data.self_cost_list,
+                    fill: true,
+                    borderColor: "#dc3545",
+                  },
+                  
+                ],
+              }
+
+              this.profitData = {
+                labels: this.data.date_list,
+                datasets: [
+                  {
+                    label: this.$t('Прибыль'),
+                    data: this.data.profit_list,
+                    fill: true,
+                    borderColor: "#198754",
+                  },
+
+                  
+                ],
+              }
+
+
+              this.options = {
+                plugins: {
+                  legend: {
+                    display: false,
+                  },
+                },
+              }
+            })
+            
+            
+        })
+
+        this.axios.get('/api/reports/profit_by_category/', {params: {start_date: startDate, end_date:endDate, shop_id: this.current_shop}}).then((response) => {
+          let category_data
+          Promise.resolve(category_data = response.data['data']).then(() => {
+            this.profitDataByCategory = {
+              labels: category_data.categories_list,
+                datasets: [
+                  {
+                    data: category_data.profit_list,
+                    backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
+                  },
+                  
+                ],
+            }
+
+            this.categoryOptions = {
+                plugins: {
+                  legend: {
+                    position: 'bottom'
+                  },
+                },
+              }
+
             this.emitter.emit("isLoading", false);
+          })
         })
       },
      format(date){
